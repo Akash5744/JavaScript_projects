467f631d731e2a2b22d86cea6cfbf40c2ee98c72
Akash5744 <110530348+Akash5744@users.noreply.github.com>
2022-08-03 14:42:26 +0530

    Update script.js

--- color_fliper/script.js
+++ color_fliper/script.js
@@ -4,10 +4,13 @@ const color = ['red','green','white','orange','yellow','blue','black','magenta']
 Array.from(color).forEach((element)=>{
     console.log(element)
 })
-
+    let i = 0;
     document.getElementById('btn').addEventListener('click',()=>{
             
-        container.style.backgroundColor = 'rgb(255,0,0)';
+        container.style.backgroundColor = color[i];
+        i++;
+        if(i > color.length)
+            i = 0;
             
     })
 
